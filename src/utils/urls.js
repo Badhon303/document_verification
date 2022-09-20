8305204e34bad6f75fc2111cda75926259671b39
Badhon303 <badhon.alam303@gmail.com>
2022-09-20 23:48:32 +0600

    ip fixed

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -5,5 +5,5 @@ export const BASE_URL =
 
 export const QR_URL =
   process.env.REACT_APP_ENV === "dev"
-    ? "http://115.127.24.185:3000"
+    ? "http://44.202.8.142:3000"
     : "http://localhost:3000"
